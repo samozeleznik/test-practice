65ba2aae9c0ba2643d187d7141a5e8e33220fea1
Samo <samo.zeleznik5@gmail.com>
2015-05-07 21:03:32 +0200

    added tasks model, added accessor for tasks to the project model and changed the done? method so it returns true if the project has no tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507184726) do
+ActiveRecord::Schema.define(version: 20150507185819) do
 
   create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Project do
 		project = Project.new
 		expect(project.done?).to be_truthy
 	end
+	
+	it "knows that a project with an incomplete task is not done" do
+		project = Project.new
+		task = Task.new
+		project.tasks << task
+		expect(project.done?).to be_falsy
+	end
 end	
