06e0e619d20f84b14d611a72e3eb3419b4f99a8a
Samo <samo.zeleznik5@gmail.com>
2015-05-07 21:08:00 +0200

    refactored test so it initializes the project and task object only once

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Project do
-	it "considers a project with no tasks to be done" do
-		project = Project.new
-		expect(project.done?).to be_truthy
-	end
-	
-	it "knows that a project with an incomplete task is not done" do
-		project = Project.new
-		task = Task.new
-		project.tasks << task
-		expect(project.done?).to be_falsy
+	describe "Initialization" do
+	let(:project) { Project.new }
+	let(:task) { Task.new }
+
+		it "considers a project with no tasks to be done" do
+			expect(project.done?).to be_truthy
+		end
+		
+		it "knows that a project with an incomplete task is not done" do
+			project.tasks << task
+			expect(project.done?).to be_falsy
+		end
 	end
 end	
