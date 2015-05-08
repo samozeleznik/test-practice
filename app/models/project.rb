04c5ca2358e473a40580eeda244331f6b09f057a
Samo <samo.zeleznik5@gmail.com>
2015-05-08 17:04:09 +0200

    extracted duplicated data (21 days) into a constant class method velocity_length_in_days

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,10 @@ class Project
 		@tasks = []
 	end
 
+  def self.velocity_length_in_days
+    21
+  end
+
 	def incomplete_tasks 
 		tasks.reject(&:completed_at)
 	end
@@ -26,7 +30,7 @@ class Project
   end
   
   def current_rate
-    completed_velocity * 1.0 / 21
+    completed_velocity * 1.0 /Project.velocity_length_in_days
   end
 
   def projected_days_remaining
