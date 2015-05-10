b374d9c612e6a4acfed17729ef1001b72944ff3f
Samo <samo.zeleznik5@gmail.com>
2015-05-10 18:30:36 +0200

    added task and project resources and migrated DB

--- db/migrate/20150510162925_create_tasks.rb
+++ db/migrate/20150510162925_create_tasks.rb
@@ -1,6 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
+      t.references :project, index: true, foreign_key: true
+      t.string :title
+      t.integer :size
+      t.datetime :completed_at
 
       t.timestamps null: false
     end
